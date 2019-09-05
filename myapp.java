5853d65ac125e5eee6bf4e6c16409f855ef05e73
Matthew <mfarmer5102@gmail.com>
2019-09-05 00:16:07 -0400

    Initial commit

---
+++ myapp.java
@@ -0,0 +1,18 @@
+public class Application {
+
+  //Function
+  static void myMethod1() {
+    System.out.println("I like pie!");
+  }
+
+  //Function
+  static void myMethod2() {
+    System.out.println("I like cake!");
+  }
+
+  //Run program
+  public static void main(String[] args) {
+    myMethod1();
+    myMethod2();
+  }
+}
