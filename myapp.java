3d125a4e42eedaf4bbc7c5869d898183dd6bb0f7
Matthew <mfarmer5102@gmail.com>
2019-09-05 00:24:02 -0400

    Added in an if-else statement inside the for loop

--- myapp.java
+++ myapp.java
@@ -1,22 +1,34 @@
 public class Application {
 
-  // Function
+  // Pie function
   static void printILikePie() {
     System.out.println("I like pie!");
   }
 
-  // Function
+  // Cake function
   static void printILikeCake() {
     System.out.println("I like cake!");
+  }
+
+  // Spit numbers function
+  static void spitNumbers() {
+    for (int i = 0; i < 5; i++) {
+      System.out.println("Here's the number " + i);
+      if (i < 2) {
+        System.out.println("That number's less than 2.");
+      } else if (i == 2) {
+        System.out.println("That number's equal to 2!");
+      } else {
+        System.out.println("That number's greater than 2.");
+      }
+    }
   }
 
   // Run program
   public static void main(String[] args) {
     printILikePie();
     printILikeCake();
-    for (int i = 0; i < 5; i++) {
-      System.out.println("Here's the number " + i);
-    }
+    spitNumbers();
   }
 
 }
